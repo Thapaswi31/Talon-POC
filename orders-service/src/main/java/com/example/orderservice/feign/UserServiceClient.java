8c0a29d026ea4b37e505304f51a2b19f1beff4da
Thapaswi <120595583+Thapaswi31@users.noreply.github.com>
2025-07-23 16:07:52 +0530

    Add or update file: UserServiceClient.java

---
+++ orders-service/src/main/java/com/example/orderservice/feign/UserServiceClient.java
@@ -0,0 +1,12 @@
+package com.example.orderservice.feign;
+
+import com.example.orderservice.dto.UserDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-service", url = "${user-service.url}")
+public interface UserServiceClient {
+    @GetMapping("/users/{userId}")
+    UserDto getUserById(@PathVariable("userId") Long userId);
+}
